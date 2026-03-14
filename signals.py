fdd6dc2c03e55a36fb85a688b6e58022fa5666e8
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 14:50:36 +0100

    Update signals.py

--- signals.py
+++ signals.py
@@ -1,6 +1,6 @@
 import random
 
 def trading_signal():
-    signal = random.choice(["BUY","SELL","HOLD"])
-    probability = random.randint(55,90)
-    return f"📈 TRADING SIGNAL\nAction : {signal}\nProbability : {probability}%"
+    signal = random.choice(["BUY", "SELL", "HOLD"])
+    probability = random.randint(55, 90)
+    return f"📈 TRADING SIGNAL\nAction: {signal}\nProbability: {probability}%"
