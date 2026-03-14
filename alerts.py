90d177d8baa9a0c3685f2b72e2229ea66125b24d
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 14:49:36 +0100

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,15 +1,15 @@
 import random
 
-def btc_crash():
-    r = random.randint(1,15)
-    if r>12:
-        drop = random.randint(6,15)
-        return f"🚨 BTC CRASH ALERT\nDrop : -{drop}%\nHigh volatility detected"
+def crash():
+    r = random.randint(1, 15)
+    if r > 12:
+        drop = random.randint(6, 15)
+        return f"🚨 BTC CRASH ALERT\nDrop: -{drop}%"
     return "BTC stable"
 
 def whale_alert():
-    r = random.randint(1,10)
-    if r>7:
-        btc = random.randint(500,3000)
-        return f"🐋 WHALE ALERT\n{btc} BTC moved\nPossible market impact"
+    r = random.randint(1, 10)
+    if r > 7:
+        btc = random.randint(500, 3000)
+        return f"🐋 WHALE ALERT\n{btc} BTC moved"
     return "No whale detected"
