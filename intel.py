5268816b3979fc91f85932256adab88de56cb76c
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 14:12:21 +0100

    Update intel.py

--- intel.py
+++ intel.py
@@ -1,66 +1,8 @@
-import random
 import requests
 
-
-def whale_alert():
-
-    r = random.randint(1,10)
-
-    if r > 7:
-
-        btc = random.randint(500,3000)
-
-        direction = random.choice([
-            "moved to exchange",
-            "moved to cold wallet",
-            "unknown wallet transfer"
-        ])
-
-        impact = random.choice([
-            "Possible sell pressure",
-            "Possible accumulation",
-            "Market volatility expected"
-        ])
-
-        return f"""
-🐋 WHALE ALERT
-
-Volume
-{btc} BTC
-
-Movement
-{direction}
-
-Impact
-{impact}
-"""
-
-    return "🐋 No whale detected"
-
-
 def crypto_news():
-
     try:
-
-        url = "https://cryptopanic.com/api/v1/posts/?public=true"
-
-        r = requests.get(url).json()
-
-        news = r["results"][0]["title"]
-
-        return f"""
-📰 CRYPTO NEWS
-
-{news}
-
-Source
-CryptoPanic
-"""
-
+        r = requests.get("https://cryptopanic.com/api/v1/posts/?public=true").json()
+        return "📰 " + r["results"][0]["title"]
     except:
-
-        return """
-📰 CRYPTO NEWS
-
-News unavailable
-"""
+        return "News unavailable"
