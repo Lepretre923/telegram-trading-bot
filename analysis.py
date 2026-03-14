86a64c9aa6d538a92bfed17d13b38563aabe3490
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 13:06:33 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,34 +2,97 @@ import random
 
 def market_analysis():
 
-    rsi=random.randint(30,70)
+    rsi = random.randint(30,70)
 
-    macd=random.choice(["Bullish","Bearish"])
+    macd = random.choice(["Bullish","Bearish"])
 
-    sentiment=random.choice(["Bullish","Neutral","Bearish"])
+    sentiment = random.choice(["Bullish","Neutral","Bearish"])
 
-    score=rsi
+    momentum = random.randint(-5,5)
 
-    if macd=="Bullish":
-        score+=10
+    score = rsi
+
+    if macd == "Bullish":
+        score += 10
     else:
-        score-=10
+        score -= 10
 
-    if sentiment=="Bullish":
-        score+=10
-    elif sentiment=="Bearish":
-        score-=10
+    if sentiment == "Bullish":
+        score += 10
+    elif sentiment == "Bearish":
+        score -= 10
+
+    if momentum > 0:
+        score += 5
+    else:
+        score -= 5
+
+    if score > 100:
+        score = 100
+
+    if score < 0:
+        score = 0
+
+
+    trend = "Neutral"
+
+    if score > 65:
+        trend = "Bullish trend"
+
+    if score < 40:
+        trend = "Bearish trend"
+
+
+    probability = random.randint(50,85)
+
 
     return f"""
 📊 AI MARKET ANALYSIS
 
-RSI : {rsi}
-MACD : {macd}
-Sentiment : {sentiment}
+RSI
+{rsi}
 
-Market Score : {score}/100
+MACD
+{macd}
+
+Sentiment
+{sentiment}
+
+Momentum
+{momentum}
+
+━━━━━━━━━━━━━━
+
+Market Score
+{score}/100
+
+Trend
+{trend}
+
+Probability
+{probability} %
+
+━━━━━━━━━━━━━━
+
+Guide
+
+Score > 65
+Bullish market structure
+
+Score 40 - 65
+Neutral market
+
+Score < 40
+Bearish pressure
 """
+
 
 def btc_chart():
 
-    return "📈 BTC CHART\nhttps://www.tradingview.com/chart/?symbol=BINANCE:BTCUSDT"
+    return """
+📈 BTC CHART
+
+TradingView
+
+https://www.tradingview.com/chart/?symbol=BINANCE:BTCUSDT
+"""
