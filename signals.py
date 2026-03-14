c0885c91c4f218280d68e7f58ee4d572d5690720
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 21:19:49 +0100

    Delete signals.py

--- signals.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def trading_signal():
-    signal = random.choice(["BUY", "SELL", "HOLD"])
-    probability = random.randint(55, 90)
-    return f"📈 TRADING SIGNAL\nAction: {signal}\nProbability: {probability}%"
