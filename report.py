6c4da4f52b7a1e77ffe47bfbff4262a190522767
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 14:55:38 +0100

    Update report.py

--- report.py
+++ report.py
@@ -1,4 +1,4 @@
-from analysis import market_analysis
+from analysis import market_analysis, btc_chart
 from signals import trading_signal
 from intel import crypto_news
 
@@ -11,4 +11,6 @@ def full_report():
 {trading_signal()}
 
 {crypto_news()}
+
+{btc_chart()}
 """
