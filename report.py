cbdda55be80f66acbc015886d1359311122b5404
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 14:48:21 +0100

    Update report.py

--- report.py
+++ report.py
@@ -3,4 +3,12 @@ from signals import trading_signal
 from intel import crypto_news
 
 def full_report():
-    return f"📋 GLOBAL MARKET REPORT\n\n{market_analysis()}\n{trading_signal()}\n{crypto_news()}"
+    return f"""
+📋 GLOBAL MARKET REPORT
+
+{market_analysis()}
+
+{trading_signal()}
+
+{crypto_news()}
+"""
