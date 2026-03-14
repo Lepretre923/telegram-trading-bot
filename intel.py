31e6b23fe77065bf9a51aabb00acccd4a13c3d9c
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 21:17:58 +0100

    Delete intel.py

--- intel.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def crypto_news():
-    try:
-        r = requests.get("https://cryptopanic.com/api/v1/posts/?public=true").json()
-        return "📰 " + r["results"][0]["title"]
-    except:
-        return "News unavailable"
