3f4512fe40ac62e4e7ed9a07c867a4742f90e55b
Lepretre923 <nourqg2.0@gmail.com>
2026-03-14 13:05:25 +0100

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -2,17 +2,47 @@ import random
 
 def btc_crash():
 
-    r=random.randint(1,15)
+    risk = random.randint(1,100)
 
-    if r>12:
+    if risk > 80:
 
-        drop=random.randint(6,15)
+        drop = random.randint(6,15)
+
+        support1 = random.randint(62000,67000)
+        support2 = support1 - random.randint(1000,3000)
 
         return f"""
 🚨 BTC CRASH ALERT
 
-Drop : -{drop}%
-High volatility detected
+Probabilité crash
+{risk} %
+
+Correction estimée
+
+-{drop} %
+
+Zones de support
+
+Support critique
+{support1} $
+
+Support majeur
+{support2} $
+
+Analyse
+
+Volatilité élevée détectée.
+Risque de liquidation massive.
+
+Guide
+
+Surveiller réaction
+sur les supports.
 """
 
-    return "BTC stable"
+    return """
+BTC stable
+
+Aucun signal de crash
+détecté actuellement.
+"""
